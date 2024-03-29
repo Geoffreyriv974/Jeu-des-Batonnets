8d07a8304567710ac56804d990a5a70fb6c5026f
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-03-29 12:27:20 +0100

    Ajout de la fin de partie

--- index.js
+++ index.js
@@ -1,62 +1,58 @@
-//supprimer 1 éléments
 document.addEventListener("DOMContentLoaded", function() {
-    var deleteElement = document.getElementById("btn_1");
-    var plateau = document.getElementById("plateau");
 
-    deleteElement.addEventListener("click", function() {
+    const deleteElement1 = document.getElementById("btn_1");
+    const deleteElement2 = document.getElementById("btn_2");
+    const deleteElement3 = document.getElementById("btn_3");
+    const plateau = document.getElementById("plateau");
 
-        var lastSVG = plateau.lastElementChild;
+    // Fonction pour supprimer les bâtonnets
+    function remove(numberBaton) {
+        for(let i = 0; i < numberBaton; i++) {
+            const lastSVG = plateau.lastElementChild;
 
-        if (lastSVG) {
-            plateau.removeChild(lastSVG);
-        } else {
-            alert("Il n'y a plus d'élément SVG à supprimer !");
-        }
-    });
-});
-
-//supprimer 2 éléments
-document.addEventListener("DOMContentLoaded", function() {
-    var deleteElement = document.getElementById("btn_2");
-    var plateau = document.getElementById("plateau");
-
-    deleteElement.addEventListener("click", function() {
-
-        for (let i = 0; i < 2; i++) {
-            var lastSVG = plateau.lastElementChild;
             if (lastSVG) {
-                plateau.removeChild(lastSVG)
+                plateau.removeChild(lastSVG);
             } else {
                 break;
             }
         }
-    });
-});
+        if (plateau.childElementCount === 1 && plateau.childElementCount === 0) {
 
-//supprimer 3 éléments
-document.addEventListener("DOMContentLoaded", function() {
-    var deleteElement = document.getElementById("btn_3");
-    var plateau = document.getElementById("plateau");
+            const playerWin = document.getElementById("h3")
 
-    deleteElement.addEventListener("click", function() {
+            playerWin.innerText = "Le " + playerContainer + " a gagner !"
 
-        for (let i = 0; i < 3; i++) {
-            var lastSVG = plateau.lastElementChild;
-            if (lastSVG) {
-                plateau.removeChild(lastSVG)
-            } else {
-                break;
-            }
+            deleteElement1.disabled = true;
+            deleteElement2.disabled = true;
+            deleteElement3.disabled = true;
         }
-    });
-
-    var Player = document.getElementById("player")
-
-    if (Player === "Joueur-1") {
-        Player.textContent = "Joueur-2"
     }
 
+    deleteElement1.addEventListener("click", function () {
+        remove(1);
+    })
+    deleteElement2.addEventListener("click", function () {
+        remove(2);
+    })
+    deleteElement3.addEventListener("click", function () {
+        remove(3);
+    })
+
+    const btn = document.getElementsByClassName("btn");
+    const tabPlayer = ["Player-1" ,"Player-2"];
+    const playerContainer = document.getElementById("name-player").innerText;
+
+    // Affichage du joueur
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener("click", function() {
+            const namePlayer = playerContainer.innerText;
+            let indexOfPlayer = tabPlayer.indexOf(namePlayer);
+
+            if (indexOfPlayer === 0) {
+                playerContainer.innerText = tabPlayer[tabPlayer.length - 1];
+            } else {
+                playerContainer.innerText = tabPlayer[indexOfPlayer - 1];
+            }
+        });
+    }
 });
-
-
-
